5ef74f62ba6d97264f8e615235ce66c2273c332c
Maxim Seredkin <m.seredkin@white-soft.ru>
2021-06-22 15:33:47 +1000

    Тест контейнер постгри для теста поднятия контекста

--- src/test/java/ru/thewhite/edu/eduserver/EduServerApplicationTests.java
+++ src/test/java/ru/thewhite/edu/eduserver/EduServerApplicationTests.java
@@ -1,9 +1,11 @@
 package ru.thewhite.edu.eduserver;
 
+import com.jupiter.tools.spring.test.postgres.annotation.EnablePostgresTestContainers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnablePostgresTestContainers
 class EduServerApplicationTests {
 
 	@Test
