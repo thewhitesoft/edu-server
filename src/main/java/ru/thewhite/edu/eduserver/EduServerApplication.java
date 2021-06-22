dabd02fcb3fc943c200530d35b6cadb2714542c9
Maxim Seredkin <m.seredkin@white-soft.ru>
2021-06-22 11:34:07 +1000

    start.spring.io template

---
+++ src/main/java/ru/thewhite/edu/eduserver/EduServerApplication.java
@@ -0,0 +1,13 @@
+package ru.thewhite.edu.eduserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/thewhite/edu/eduserver/EduServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.thewhite.edu.eduserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
